399fa72f804c4caf274ebe90669f157dbe7ad58e
Es_lam <81473638+eslamredahassan@users.noreply.github.com>
2025-02-17 20:21:14 +0200

    Update index.d.ts

--- build/index.d.ts
+++ build/index.d.ts
@@ -9,7 +9,6 @@ export declare class PappuZydenMusicCard {
         progress?: number;
         startTime?: string;
         endTime?: string;
-        duration?: string; // <-- Added duration here
     });
 
     public setName(name: string): this;
@@ -21,7 +20,6 @@ export declare class PappuZydenMusicCard {
     public setProgress(progress: number): this;
     public setStartTime(starttime: string): this;
     public setEndTime(endtime: string): this;
-    public setDuration(duration: string): this; // <-- Added this method
 
     public build(): Promise<Buffer>;
 }
